ca3ce84727f2d183f1145671b1bcf1a614fba2fe
Ayaz Araf <ayaz.araf@gmail.com>
2020-03-17 19:46:23 +0000

    Automatically backed up by Learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -1,11 +1,11 @@
 # Define display_board that accepts a board and prints
 # out the current state.
 def display_board(board)
-  puts " #{boards[0]} | #{boards[1]} | #{boards[2]} "
+  puts " #{board[0]} | #{board[1]} | #{board[2]} "
   puts "-----------"
-  puts " #{boards[3]} | #{boards[4]} | #{boards[5]} "
+  puts " #{board[3]} | #{board[4]} | #{board[5]} "
   puts "-----------"
-  puts " #{boards[6]} | #{boards[7]} | #{boards[8]} "
+  puts " #{board[6]} | #{board[7]} | #{board[8]} "
 end
 board = [" "," "," "," "," "," "," "," "," "]
 board[0]="X"
